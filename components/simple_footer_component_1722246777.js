a293da6ea0022de9fed4ebbdf097f5a8c03dec2a
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-29 09:53:06 +0000

    Placeholder text replacement

--- components/simple_footer_component_1722246777.js
+++ components/simple_footer_component_1722246777.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722246777", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            OfferPad - Sell Your Home Instantly
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        OfferPad is a modern real estate platform that allows homeowners to get a fair cash offer and sell their house quickly and easily. Our technology streamlines the process, providing a convenient and stress-free way to sell your home.
                     </div>
                 </div>
             </div>
